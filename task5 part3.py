aa5d7861629c9d5250f0588b832d887c348709d1
vdi-terminal <vdi-terminal@EDU-WSB.local>
2025-05-09 14:21:21 +0200

    Revert "Update task5 part3.py"
    This reverts commit 3d0897c69c67d9bcc54416f28ca3b82b47fd973e.

--- task5 part3.py
+++ task5 part3.py
@@ -1,9 +1,1 @@
 print("Hello, file 3")
-def divide_two_numbers(a, b):
-    if b == 0:
-        return "Error: Cant divided by 0."
-    return a / b
-
-if __name__ == "__main__":
-    result = divide_two_numbers(10, 2) 
-    print(f"divide: {result}")
